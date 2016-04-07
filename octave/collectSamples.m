b8a1afbe7959ba5a91025a842c51bde37f5dd451
micheleguerriero <michele.guerriero2391@gmail.com>
2016-04-07 19:41:57 +0200

    Added sequential sampling of operational data.

---
+++ octave/collectSamples.m
@@ -0,0 +1,28 @@
+function [clean_sample, clean_sample_nCores]  = colletSamples (directory, nSlots, timestep)
+
+if (! ischar (directory))
+  error ("read_from_directory: DIRECTORY should be a string");
+endif
+
+files = glob ([directory, "/*.csv"]);
+
+sample = [];
+
+for ii = 1:numel (files)
+  file = files{ii};
+  last_sample = read_data (file);
+  %% here we assume that each operational file has the same 
+  %% number or rows ( which basically means that each 
+  %% real experiment is performed sampling the same
+  %% number of observations.
+  window_size = size (last_sample)(1) ./ nSlots;
+  current_sample = last_sample [window_size .* (timestep - 1) : window_size .* (timestep), :]
+  sample = [sample; current_sample];
+endfor
+
+[clean_sample, indices] = clear_outliers (sample);
+
+clean_sample_nCores = clean_sample;
+clean_sample_nCores(:, end) = 1 ./ clean_sample_nCores(:, end);
+
+endfunction
--- octave/main.m
+++ octave/main.m
@@ -2,49 +2,87 @@ clear all
 close all hidden
 clc
 
-%% Input data parameters
-query = "analyt";
+%% Input data parameters: path to folder containing 
+%% the analytical data and to folder containing 
+%% the operational data.
+query_analytical_data = "analyt";
+query_operational_data = "oper";
 base_dir = "/Users/michele/MATLAB/bootstrapped-hadoop-svr/source_data/";
 
 % Splitting parameters
 train_frac = 0.6;
 test_frac = 0.2;
 
-% Number of iterations for the re-training after the bootstrapping
-iterations = 10;
+%% Number of iterations for the re-training after the bootstrapping
+iterations = 1;
 
-%% Initializing the Knowledge Base
-[sample, sample_nCore] = initKB ([base_dir, query]);
+%% Initializing the Knowledge Base sampling the analytical data
+[analytical_sample, analytical_sample_nCore] = initKB ([base_dir, query_analytical_data]);
 
-% Scaling and permutating the dataset
+%% Scaling and permutating the analytical dataset
 rand ("seed", 17);
-permutation = randperm (size (sample, 1));
+permutation = randperm (size (analytical_sample, 1));
 
-[scaled, mu, sigma] = zscore (sample);
-shuffled = scaled(permutation, :);
+[analytical_scaled, mu, sigma] = zscore (analytical_sample);
+analytical_shuffled = analytical_scaled(permutation, :);
 
 mu_y = mu(1);
 sigma_ = sigma(1);
 mu_X = mu(2:end);
 sigma_X = sigma(2:end);
 
-[scaled_nCores, mu, sigma] = zscore (sample_nCore);
-shuffled_nCores = scaled_nCores(permutation, :);
+[analytical_scaled_nCores, mu, sigma] = zscore (analytical_sample_nCore);
+analytical_shuffled_nCores = analytical_scaled_nCores(permutation, :);
 
 mu_X_nCores = mu(2:end);
 sigma_X_nCores = sigma(2:end);
 
-% Separating prediction variables from target variable
-y = shuffled(:, 1);
-X= shuffled(:, 2:end);
+%% Separating prediction variables from target variable in the analytical dataser
+y = analytical_shuffled(:, 1);
+X= analytical_shuffled(:, 2:end);
 
-y_nCores = shuffled_nCores(:, 1);
-X_nCores = shuffled_nCores(:, 2:end);
+y_nCores = analytical_shuffled_nCores(:, 1);
+X_nCores = analytical_shuffled_nCores(:, 2:end);
 
-% Splitting the datasets
+%% Splitting the analytical datasets
 [ytr, ytst, ycv] = split_sample (y, train_frac, test_frac);
 [Xtr, Xtst, Xcv] = split_sample (X, train_frac, test_frac);
 [ytr_nCores, ytst_nCores, ycv_nCores] = split_sample (y_nCores, train_frac, test_frac);
 [Xtr_nCores, Xtst_nCores, Xcv_nCores] = split_sample (X_nCores, train_frac, test_frac);
 
+%% TODO: train the machine learner with the obtainted
+%% knowlege base.
+
+%%
+for ii = 1: iterations
+  
+  %% sampling the operational data for the current iteration.
+  [operational_sample, operational_sample_nCore] = collectSamples ([base_dir, query_operational_data], iterations, ii)
+  
+  %% Scaling and permutating the operational dataset 
+  %% for the current iteration
+  rand ("seed", 17);
+  permutation = randperm (size (operational_sample, 1));
+
+  [operational_scaled, mu, sigma] = zscore (operational_sample);
+  operational_shuffled = operational_scaled(permutation, :);
+
+  mu_y = mu(1);
+  sigma_ = sigma(1);
+  mu_X = mu(2:end);
+  sigma_X = sigma(2:end);
+
+  [operational_scaled_nCores, mu, sigma] = zscore (operational_sample_nCore);
+  operational_shuffled_nCores = operational_scaled_nCores(permutation, :);
+  
+  %% TODO: updating the knowledge base [analytical_shuffled] 
+  %% and [analytical_shuffled_nCore] with the operational sample
+  %% for the current iteration
+  
+  %% TODO: re-train the machine learner with the updated
+  %% knowlege base. 
+  
+  %% TODO: evaluate ( generate also some plots ) the current accuracy 
+  %% of the machine learner
+endfor
 
