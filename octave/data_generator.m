dd2765424b44ae4db74b23eb0d63c75127ce72a2
micheleguerriero <michele.guerriero2391@gmail.com>
2016-04-19 17:37:13 +0200

    Updating the data generator.

--- octave/data_generator.m
+++ octave/data_generator.m
@@ -3,6 +3,7 @@
 clear all
 close all hidden
 clc
+rand ("seed", 17)
 
 output_file_analyt = "/Users/michele/Desktop/corsi_phd/Quantitative/progetto/bootstrapped-hadoop-svr/source_data/analyt/dataAM.csv";
 output_file_oper_1 = "/Users/michele/Desktop/corsi_phd/Quantitative/progetto/bootstrapped-hadoop-svr/source_data/oper/dataOper1.csv";
@@ -11,27 +12,27 @@ output_file_oper_3 = "/Users/michele/Desktop/corsi_phd/Quantitative/progetto/boo
 
 
 f = @(x) exp(-x ./ 2) .* 10000;
-input_range = 1:20;
+input_range = 1:120;
 output_values = f (input_range);
 input_range = input_range (:);
 output_values = output_values (:);
 
 output_values_oper1 = output_values;
-output_values_oper1(3:6) = output_values(3:6) + rand(4, 1)*500;
+output_values_oper1(18:36) = output_values(18:36) + rand(4, 1)*500;
 result = [output_values_oper1, input_range];
 dlmwrite (output_file_oper_1,result);
 
 output_values_oper2 = output_values;
-output_values_oper2(3:6) = output_values(3:6) + rand(4, 1)*350;
+output_values_oper2(18:36) = output_values(18:36) + rand(4, 1)*350;
 result = [output_values_oper2, input_range];
 dlmwrite (output_file_oper_2,result);
 
 output_values_oper3 = output_values;
-output_values_oper3(3:6) = output_values(3:6) + rand(4, 1)*650;
+output_values_oper3(18:36) = output_values(18:36) + rand(4, 1)*650;
 result = [output_values_oper3, input_range];
 dlmwrite (output_file_oper_3,result);
 
 output_values_analyt = output_values;
-output_values_analyt(3:6) = output_values_analyt(3:6) + rand(4, 1)*1200;
+output_values_analyt(18:36) = output_values_analyt(18:36) + rand(4, 1)*1200;
 result_analyt = [output_values_analyt, input_range];
 dlmwrite (output_file_analyt,result_analyt);
