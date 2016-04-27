28650a8f0a44a47f68452bbfb971140b4f68370a
Irma Metra <metra.irma@gmail.com>
2016-04-27 11:33:46 +0200

    added parameter selection with cross-validation

---
+++ octave/parameter_selection.m
@@ -0,0 +1,20 @@
+## -*- texinfo -*- 
+## @deftypefn {Function File} {[@var{responseTime}, @var{inst}] =} parameter_selection (@var{responseTime}, @var{inst})
+##
+## Perform cross validation to find best parameters using label @var{responseTime} and feature @var{nCores}
+## Best parameters are in @var{bestcv}, @var{bestc}, @var{bestg}
+##
+## @end deftypefn
+
+function [bestcv,bestc,bestg] = parameter_selection (responseTime, nCores)
+bestcv = 0;
+for log2c = -1:3,
+  for log2g = -4:1,
+    cmd = ['-v 5 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
+    cv = svmtrain(responseTime, nCores, cmd);
+    if (cv >= bestcv),
+      bestcv = cv; bestc = 2^log2c; bestg = 2^log2g;
+    end
+    fprintf('%g %g %g (best c=%g, g=%g, rate=%g)\n', log2c, log2g, cv, bestc, bestg, bestcv);
+  end
+end
