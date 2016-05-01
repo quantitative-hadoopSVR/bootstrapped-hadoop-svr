83c0cf504a0d06f0788e3c97d141df81f036240d
Irma Metra <metra.irma@gmail.com>
2016-05-01 21:17:29 +0200

    Added description to plots

--- octave/save_plots.m
+++ octave/save_plots.m
@@ -2,11 +2,34 @@ function save_plots(model, model_linear, RMSEs, RMSEs_linear, MSE, MSE_linear)
 %% save plots
 
 path = "../plots/";
-saveas(plot(RMSEs),[path,"RMSEs_Kernel.jpg"]);
-saveas(plot(MSE),[path,"MSE_Kernel.jpg"]);
-saveas(plot(model.SVs),[path,"SupportVectors_Kernel.jpg"]);
-saveas(plot(RMSEs_linear),[path,"RMSEs_linear.jpg"]);
-saveas(plot(MSE_linear),[path,"MSE_linear.jpg"]);
-saveas(plot(model_linear.SVs),[path,"SupportVectors_Linear.jpg"]);
+
+h1=plot(MSE_linear);
+title("Mean Square Error for each iteration, Linear Model");
+xlabel ("Iterations");
+ylabel ("MSE");
+saveas(h1,[path,"MSE_linear.jpg"]);
+
+h2=plot(MSE);
+title("Mean Square Error for each iteration, Kernel Model");
+xlabel ("Iterations");
+ylabel ("MSE");
+saveas(h2,[path,"MSE_Kernel.jpg"]);
+
+h3=plot(RMSEs_linear);
+title("Root Mean Square Error for each iteration, Linear Model");
+xlabel ("Iterations");
+ylabel ("RMSE");
+saveas(h3,[path,"RMSEs_linear.jpg"]);
+
+h4=plot(RMSEs);
+title("Root Mean Square Error for each iteration, Kernel Model");
+xlabel ("Iterations");
+ylabel ("RMSE");
+saveas(h4,[path,"RMSEs_Kernel.jpg"]);
+
+
+
+%saveas(plot(model.SVs),[path,"SupportVectors_Kernel.jpg"]);
+%saveas(plot(model_linear.SVs),[path,"SupportVectors_Linear.jpg"]);
 
 endfunction
