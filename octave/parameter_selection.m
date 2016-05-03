35ffe237664d83ffcf2780556a077766ade3d21f
Irma Metra <metra.irma@gmail.com>
2016-05-03 21:55:27 +0200

    model with crossvalidation

--- octave/parameter_selection.m
+++ octave/parameter_selection.m
@@ -6,15 +6,15 @@
 ##
 ## @end deftypefn
 
-function [bestcv,bestc,bestg] = parameter_selection (responseTime, nCores)
+function [bestcv,bestc,bestg] = parameter_selection (responseTime, nCores, wtrain)
 bestcv = 0;
 for log2c = -1:3,
   for log2g = -4:1,
     cmd = ['-v 5 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
-    cv = svmtrain(responseTime, nCores, cmd);
+    cv = svmtrain(wtrain, responseTime, nCores, cmd);
     if (cv >= bestcv),
       bestcv = cv; bestc = 2^log2c; bestg = 2^log2g;
     end
-    fprintf('%g %g %g (best c=%g, g=%g, rate=%g)\n', log2c, log2g, cv, bestc, bestg, bestcv);
+    %%fprintf('%g %g %g (best c=%g, g=%g, rate=%g)\n', log2c, log2g, cv, bestc, bestg, bestcv);
   end
 end
