f23f81284b8b372c31768fbcfb9f781426aa63a8
Irma Metra <metra.irma@gmail.com>
2016-05-04 13:50:03 +0200

    changed crossvalidation k

--- octave/parameter_selection.m
+++ octave/parameter_selection.m
@@ -10,7 +10,7 @@ function [bestcv,bestc,bestg] = parameter_selection (responseTime, nCores, wtrai
 bestcv = 0;
 for log2c = -1:3,
   for log2g = -4:1,
-    cmd = ['-v 5 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
+    cmd = ['-v 4 -c ', num2str(2^log2c), ' -g ', num2str(2^log2g)];
     cv = svmtrain(wtrain, responseTime, nCores, cmd);
     if (cv >= bestcv),
       bestcv = cv; bestc = 2^log2c; bestg = 2^log2g;
